4b945ddcb8e6acfa17e2ddffeb519b93bac2895d
Aiman Ibn Ubayd <aimanibnubayd@gmail.com>
2024-03-11 02:54:04 +0600

    sola

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from "react";
 
-const Branding: FC = ({ image, title, message }) => {
+interface BrandingProps {
+  image: string;
+  title: string;
+  message: string;
+}
+
+const Branding: FC<BrandingProps> = ({ image, title, message }) => {
   return (
     <div className="ps-4 hidden py-4 lg:block">
-      <div className="relative mt-3 h-full w-full overflow-hidden rounded-xl">
+      <div className="relative h-full w-full overflow-hidden rounded-xl">
         <img
           src={`assets/images/ai/${image}.jpg`}
           alt=""
@@ -11,13 +17,9 @@ const Branding: FC = ({ image, title, message }) => {
         />
 
         <div className="bg-default-950/40 absolute inset-0">
-          <div className="flex h-full items-end justify-center">
-            <div className="text-start p-6 ">
-              <h5 className="mb-3 text-xl font-bold text-white">
-                Solana Token Creator
-                <br />
-                {title}
-              </h5>
+          <div className="fles h-full items-end justify-center">
+            <div className="text-start p-6">
+              <h5 className="mb-3 text-xl font-bold text-white">{title}</h5>
               <p className="text-default-400 text-base font-medium">
                 {message}
               </p>
